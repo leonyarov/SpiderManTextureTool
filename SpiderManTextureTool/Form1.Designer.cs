7a39afd49f543ff7987948a3bde67afc4b47b8ac
leon-yarov <leon44ml@gmail.com>
2022-08-16 10:01:00 +0300

    Buttons logic

--- SpiderManTextureTool/Form1.Designer.cs
+++ SpiderManTextureTool/Form1.Designer.cs
@@ -31,25 +31,28 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.texturePathTxt = new System.Windows.Forms.TextBox();
+            this.selectTextureBtn = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textureSizeCmb = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textureFormatCmb = new System.Windows.Forms.ComboBox();
+            this.convertToDDSBtn = new System.Windows.Forms.Button();
+            this.openTexture = new System.Windows.Forms.OpenFileDialog();
+            this.saveTexture = new System.Windows.Forms.SaveFileDialog();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.ddsPathTxt = new System.Windows.Forms.TextBox();
+            this.selectDDSBtn = new System.Windows.Forms.Button();
+            this.convertDDSBtn = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.saveDDS = new System.Windows.Forms.SaveFileDialog();
+            this.saveToChk = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.groupBox1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -57,6 +60,7 @@
             this.tableLayoutPanel1.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.flowLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -65,37 +69,38 @@
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(3, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(797, 74);
+            this.groupBox1.Size = new System.Drawing.Size(902, 74);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Original .texture file";
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Controls.Add(this.textBox1);
-            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.texturePathTxt);
+            this.flowLayoutPanel1.Controls.Add(this.selectTextureBtn);
             this.flowLayoutPanel1.Controls.Add(this.label3);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 19);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(791, 52);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(896, 52);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
-            // textBox1
+            // texturePathTxt
             // 
-            this.textBox1.Location = new System.Drawing.Point(3, 3);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(703, 23);
-            this.textBox1.TabIndex = 0;
+            this.texturePathTxt.Location = new System.Drawing.Point(3, 3);
+            this.texturePathTxt.Name = "texturePathTxt";
+            this.texturePathTxt.Size = new System.Drawing.Size(703, 23);
+            this.texturePathTxt.TabIndex = 0;
             // 
-            // button1
+            // selectTextureBtn
             // 
-            this.button1.Location = new System.Drawing.Point(712, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Select";
-            this.button1.UseVisualStyleBackColor = true;
+            this.selectTextureBtn.Location = new System.Drawing.Point(712, 3);
+            this.selectTextureBtn.Name = "selectTextureBtn";
+            this.selectTextureBtn.Size = new System.Drawing.Size(75, 23);
+            this.selectTextureBtn.TabIndex = 1;
+            this.selectTextureBtn.Text = "Select";
+            this.selectTextureBtn.UseVisualStyleBackColor = true;
+            this.selectTextureBtn.Click += new System.EventHandler(this.selectTextureBtn_Click);
             // 
             // label3
             // 
@@ -113,7 +118,7 @@
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox2.Location = new System.Drawing.Point(3, 77);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(797, 121);
+            this.groupBox2.Size = new System.Drawing.Size(902, 121);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Convert to .dds";
@@ -121,12 +126,12 @@
             // flowLayoutPanel2
             // 
             this.flowLayoutPanel2.Controls.Add(this.tableLayoutPanel1);
-            this.flowLayoutPanel2.Controls.Add(this.button2);
+            this.flowLayoutPanel2.Controls.Add(this.tableLayoutPanel2);
             this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 19);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(791, 99);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(896, 99);
             this.flowLayoutPanel2.TabIndex = 4;
             // 
             // tableLayoutPanel1
@@ -136,9 +141,9 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textureSizeCmb, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.comboBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textureFormatCmb, 1, 1);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
@@ -158,19 +163,19 @@
             this.label1.Text = "Texture Size:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // comboBox1
+            // textureSizeCmb
             // 
-            this.comboBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.textureSizeCmb.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textureSizeCmb.FormattingEnabled = true;
+            this.textureSizeCmb.Items.AddRange(new object[] {
             "Auto",
             "512",
             "1024",
             "2048"});
-            this.comboBox1.Location = new System.Drawing.Point(80, 3);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 23);
-            this.comboBox1.TabIndex = 3;
+            this.textureSizeCmb.Location = new System.Drawing.Point(80, 3);
+            this.textureSizeCmb.Name = "textureSizeCmb";
+            this.textureSizeCmb.Size = new System.Drawing.Size(121, 23);
+            this.textureSizeCmb.TabIndex = 3;
             // 
             // label2
             // 
@@ -183,11 +188,11 @@
             this.label2.Text = "Format:";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // comboBox2
+            // textureFormatCmb
             // 
-            this.comboBox2.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
+            this.textureFormatCmb.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.textureFormatCmb.FormattingEnabled = true;
+            this.textureFormatCmb.Items.AddRange(new object[] {
             "BC1",
             "BC2",
             "BC3",
@@ -201,23 +206,25 @@
             "DXT3",
             "DXT5",
             "DX10"});
-            this.comboBox2.Location = new System.Drawing.Point(80, 32);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 23);
-            this.comboBox2.TabIndex = 3;
+            this.textureFormatCmb.Location = new System.Drawing.Point(80, 32);
+            this.textureFormatCmb.Name = "textureFormatCmb";
+            this.textureFormatCmb.Size = new System.Drawing.Size(121, 23);
+            this.textureFormatCmb.TabIndex = 3;
             // 
-            // button2
+            // convertToDDSBtn
             // 
-            this.button2.Location = new System.Drawing.Point(3, 67);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Convert";
-            this.button2.UseVisualStyleBackColor = true;
+            this.convertToDDSBtn.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.convertToDDSBtn.Location = new System.Drawing.Point(3, 3);
+            this.convertToDDSBtn.Name = "convertToDDSBtn";
+            this.convertToDDSBtn.Size = new System.Drawing.Size(75, 23);
+            this.convertToDDSBtn.TabIndex = 1;
+            this.convertToDDSBtn.Text = "Convert";
+            this.convertToDDSBtn.UseVisualStyleBackColor = true;
+            this.convertToDDSBtn.Click += new System.EventHandler(this.convertToDDSBtn_Click);
             // 
-            // openFileDialog1
+            // openTexture
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openTexture.Filter = "Texture FIle | *.texture";
             // 
             // groupBox3
             // 
@@ -225,47 +232,47 @@
             this.groupBox3.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox3.Location = new System.Drawing.Point(3, 198);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(797, 68);
+            this.groupBox3.Size = new System.Drawing.Size(902, 68);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Convert .dds to .texture";
             // 
             // flowLayoutPanel3
             // 
-            this.flowLayoutPanel3.Controls.Add(this.textBox2);
-            this.flowLayoutPanel3.Controls.Add(this.button3);
-            this.flowLayoutPanel3.Controls.Add(this.button4);
+            this.flowLayoutPanel3.Controls.Add(this.ddsPathTxt);
+            this.flowLayoutPanel3.Controls.Add(this.selectDDSBtn);
+            this.flowLayoutPanel3.Controls.Add(this.convertDDSBtn);
             this.flowLayoutPanel3.Controls.Add(this.label4);
             this.flowLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel3.Location = new System.Drawing.Point(3, 19);
             this.flowLayoutPanel3.Name = "flowLayoutPanel3";
-            this.flowLayoutPanel3.Size = new System.Drawing.Size(791, 46);
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(896, 46);
             this.flowLayoutPanel3.TabIndex = 0;
             // 
-            // textBox2
+            // ddsPathTxt
             // 
-            this.textBox2.Location = new System.Drawing.Point(3, 3);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(618, 23);
-            this.textBox2.TabIndex = 0;
+            this.ddsPathTxt.Location = new System.Drawing.Point(3, 3);
+            this.ddsPathTxt.Name = "ddsPathTxt";
+            this.ddsPathTxt.Size = new System.Drawing.Size(618, 23);
+            this.ddsPathTxt.TabIndex = 0;
             // 
-            // button3
+            // selectDDSBtn
             // 
-            this.button3.Location = new System.Drawing.Point(627, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "Select";
-            this.button3.UseVisualStyleBackColor = true;
+            this.selectDDSBtn.Location = new System.Drawing.Point(627, 3);
+            this.selectDDSBtn.Name = "selectDDSBtn";
+            this.selectDDSBtn.Size = new System.Drawing.Size(75, 23);
+            this.selectDDSBtn.TabIndex = 1;
+            this.selectDDSBtn.Text = "Select";
+            this.selectDDSBtn.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // convertDDSBtn
             // 
-            this.button4.Location = new System.Drawing.Point(708, 3);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 1;
-            this.button4.Text = "Convert";
-            this.button4.UseVisualStyleBackColor = true;
+            this.convertDDSBtn.Location = new System.Drawing.Point(708, 3);
+            this.convertDDSBtn.Name = "convertDDSBtn";
+            this.convertDDSBtn.Size = new System.Drawing.Size(75, 23);
+            this.convertDDSBtn.TabIndex = 1;
+            this.convertDDSBtn.Text = "Convert";
+            this.convertDDSBtn.UseVisualStyleBackColor = true;
             // 
             // label4
             // 
@@ -278,11 +285,38 @@
             this.label4.Text = "Original .texture file should be present in the \'Original\' section before convers" +
     "ion";
             // 
+            // saveToChk
+            // 
+            this.saveToChk.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.saveToChk.AutoSize = true;
+            this.saveToChk.Location = new System.Drawing.Point(84, 5);
+            this.saveToChk.Name = "saveToChk";
+            this.saveToChk.Size = new System.Drawing.Size(107, 19);
+            this.saveToChk.TabIndex = 4;
+            this.saveToChk.Text = "Non Local Save";
+            this.saveToChk.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.convertToDDSBtn, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.saveToChk, 1, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 67);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(194, 29);
+            this.tableLayoutPanel2.TabIndex = 5;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(803, 274);
+            this.ClientSize = new System.Drawing.Size(908, 379);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
@@ -290,6 +324,7 @@
             this.Name = "Main";
             this.Padding = new System.Windows.Forms.Padding(3);
             this.Text = "Marvel\'s Spider-Man PC Texture tool by Leon";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.groupBox1.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.flowLayoutPanel1.PerformLayout();
@@ -301,6 +336,8 @@
             this.groupBox3.ResumeLayout(false);
             this.flowLayoutPanel3.ResumeLayout(false);
             this.flowLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -309,24 +346,27 @@
 
         private GroupBox groupBox1;
         private FlowLayoutPanel flowLayoutPanel1;
-        private TextBox textBox1;
-        private Button button1;
+        private TextBox texturePathTxt;
+        private Button selectTextureBtn;
         private GroupBox groupBox2;
         private TableLayoutPanel tableLayoutPanel1;
         private Label label1;
-        private ComboBox comboBox1;
+        private ComboBox textureSizeCmb;
         private Label label2;
-        private ComboBox comboBox2;
-        private Button button2;
+        private ComboBox textureFormatCmb;
+        private Button convertToDDSBtn;
         private FlowLayoutPanel flowLayoutPanel2;
         private Label label3;
-        private OpenFileDialog openFileDialog1;
-        private SaveFileDialog saveFileDialog1;
+        private OpenFileDialog openTexture;
+        private SaveFileDialog saveTexture;
         private GroupBox groupBox3;
         private FlowLayoutPanel flowLayoutPanel3;
-        private TextBox textBox2;
-        private Button button3;
-        private Button button4;
+        private TextBox ddsPathTxt;
+        private Button selectDDSBtn;
+        private Button convertDDSBtn;
         private Label label4;
+        private SaveFileDialog saveDDS;
+        private TableLayoutPanel tableLayoutPanel2;
+        private CheckBox saveToChk;
     }
 }
--- SpiderManTextureTool/Form1.cs
+++ SpiderManTextureTool/Form1.cs
@@ -6,5 +6,52 @@ namespace SpiderManTextureTool
         {
             InitializeComponent();
         }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //Load default combo items
+            textureSizeCmb.SelectedItem = textureSizeCmb.Items[0];
+            textureFormatCmb.SelectedItem = textureFormatCmb.Items[6];
+
+            //Load used paths
+            texturePathTxt.Text = Properties.Settings.Default.lastTexPath;
+            ddsPathTxt.Text = Properties.Settings.Default.lastDDSPath;
+
+            //Check if RawTex CMD exist
+            if (!File.Exists("RawtexCMD.exe"))
+            {
+                MessageBox.Show("No RawTex file found!");
+            }
+
+        }
+
+        private void selectTextureBtn_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == openTexture.ShowDialog())
+            {
+                Properties.Settings.Default.lastTexPath = openTexture.FileName;
+                texturePathTxt.Text = openTexture.FileName;
+            }
+
+        }
+
+        private void convertToDDSBtn_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists("RawtexCMD.exe"))
+            {
+                MessageBox.Show("No RawTex file found!");
+                return;
+            }
+
+            var texSize = textureSizeCmb.SelectedItem.ToString() == "Auto" ? "" : textureSizeCmb.SelectedItem.ToString();
+            var textureFormat = textureFormatCmb.SelectedItem.ToString()?.ToLower() ?? "bc7";
+
+            System.Diagnostics.Process.Start("RawtexCMD.exe", $"{texturePathTxt.Text} {textureFormat} {texSize} {texSize}");
+
+            if (saveToChk.Checked)
+            {
+                
+            }
+        }
     }
 }
